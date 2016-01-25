71ddfed57fb76eb0347c8b94fca5ed1e9a70e037
Ryan Roemer <ryan.roemer@formidablelabs.com>
2016-01-25 06:22:30 -0800

    Adds archetype downloads + installation.
    We use a process spawn of `npm pack` to do the heavy lifting for downloading
    archetype tarballs from file, npm, git, etc. We then expand the tarballs in
    a temp directory and process them into an outputted project.
    
    * Downloads + installs archetype templates from npm, git, file, etc. Fixes #2
    * Adds `builder-init --help`. Fixes #5
    * Adds `builder-init --version`.
    * Adds `builder-init <archetype> --prompts=JSON` helper for automation.
    * Adds usage / documenation. Fixes #6
    * Add near-functional tests for most full user interaction flows.

--- bin/builder-init.js
+++ bin/builder-init.js
@@ -3,35 +3,85 @@
 
 var path = require("path");
 var async = require("async");
+var chalk = require("chalk");
+
 var prompts = require("../lib/prompts");
-var init = require(path.resolve(__dirname, "../../builder-react-component/init.js"));
 var Templates = require("../lib/templates");
+var Task = require("../lib/task");
 
-// TODO: REMOVE AND IMPLEMENT INSTALL FROM ARCHETYPE
-// https://github.com/FormidableLabs/builder-init/issues/2
-async.auto({
-  "prompts": prompts.bind(null, init),
+/**
+ * Script runner
+ *
+ * @param {Object}    opts       Options object for Task.
+ * @param {Array}     opts.argv  Arguments array (Default: `process.argv`)
+ * @param {Object}    opts.env   Environment object to mutate (Default `process.env`)
+ * @param {Function}  callback   Callback from script run `(err, results)`.
+ * @returns {void}
+ */
+var run = module.exports = function (opts, callback) {
+  var task = new Task(opts);
+  var bin = chalk.green.bold("[builder-init]");
 
-  "templates": ["prompts", function (cb, results) {
-    var data = results.prompts;
-
-    var templates = new Templates({
-      src: path.resolve(__dirname, "../../builder-react-component/init"),
-      dest: path.resolve(process.env.HOME, "Desktop/builder-init-temp"),
-      data: data
-    });
-
-    templates.process(cb);
-  }]
-}, function (err) {
-  // TODO: REAL LOGGING
-  // https://github.com/FormidableLabs/builder-init/issues/4
-  /*eslint-disable no-console*/
-  if (err) {
-    console.error(err);
+  // Help, version, etc. - just call straight up.
+  if (!task.isInit()) {
+    return task.execute(callback);
   }
-  /*eslint-enable no-console*/
 
-  /*eslint-disable no-process-exit*/
-  process.exit(err ? err.code || 1 : 0);
-});
+  // Actual initialization.
+  async.auto({
+    download: task.execute.bind(task),
+
+    prompts: ["download", function (cb, results) {
+      process.stdout.write(
+        bin + " Preparing templates for: " + chalk.magenta(task.archName) + "\n"
+      );
+
+      prompts(results.download.init, cb);
+    }],
+
+    templates: ["prompts", function (cb, results) {
+      if (!results.prompts.destination) {
+        return cb(new Error("Destination field missing from prompts"));
+      }
+
+      var templates = new Templates({
+        src: results.download.src,
+        dest: path.resolve(results.prompts.destination),
+        data: results.prompts
+      });
+
+
+      templates.process(function (err, outFiles) {
+        if (err) { return cb(err); }
+
+        process.stdout.write(
+          "\n" + bin + " Wrote files: \n" +
+          (outFiles || []).map(function (obj) {
+            return " - " + chalk.cyan(path.relative(process.cwd(), obj.dest));
+          }).join("\n") + "\n"
+        );
+
+        cb();
+      });
+    }]
+  }, function (err, results) {
+    if (!err) {
+      process.stdout.write(
+        "\n" + bin + " New " + chalk.magenta(task.archName) + " project is ready at: " +
+        chalk.cyan(path.relative(process.cwd(), results.prompts.destination)) + "\n"
+      );
+    }
+
+    callback(err);
+  });
+};
+
+if (require.main === module) {
+  run(null, function (err) {
+    // TODO: REAL LOGGING
+    // https://github.com/FormidableLabs/builder-init/issues/4
+    if (err) { console.error(err); } // eslint-disable-line no-console
+
+    process.exit(err ? err.code || 1 : 0); // eslint-disable-line no-process-exit
+  });
+}
---
+++ test/server/spec/bin/builder-init.spec.js
@@ -0,0 +1,411 @@
+"use strict";
+
+/**
+ * These are _almost_ functional tests as we're basically invoking the entire
+ * application, just:
+ *
+ * - Mocking filesystem
+ * - Stubbing stdin to return canned responses to prompts
+ */
+var childProc = require("child_process");
+var crypto = require("crypto");
+var zlib = require("zlib");
+var stream = require("stream");
+var temp = require("temp").track();
+var _ = require("lodash");
+var Prompt = require("inquirer/lib/prompts/base");
+var _eval = require("eval");
+
+var run = require("../../../../bin/builder-init");
+var Task = require("../../../../lib/task");
+var pkg = require("../../../../package.json");
+
+var base = require("../base.spec");
+
+// Helpers
+// **Note**: It would be great to just stub stderr, stdout in beforeEach,
+// but then we don't get test output. So, we manually stub with this wrapper.
+var stdioWrap = function (fn) {
+  return function (done) {
+    base.sandbox.stub(process.stdout, "write");
+
+    var _done = function (err) {
+      process.stdout.write.restore();
+      done(err);
+    };
+
+    try {
+      return fn(_done);
+    } catch (err) {
+      return _done(err);
+    }
+  };
+};
+
+// Mock key I/O parts of the flow.
+/*eslint-disable max-statements*/
+var mockFlow = function (extracted, root) {
+  // Returned object.
+  var stubs = {};
+
+  // Fake filesystem for before and after (stubbed) extraction.
+  var hash = crypto.randomBytes(10).toString("hex");
+  var tmpDir = "tmp-dir-" + hash;
+  var fsObj = _.extend({}, root);
+  fsObj[tmpDir] = {
+    "mock-archetype-0.0.1.tgz": ""
+  };
+
+  var extractedObj = _.cloneDeep(fsObj);
+  extractedObj[tmpDir] = _.merge({}, fsObj[tmpDir], extracted ? { extracted: extracted } : null);
+
+  base.mockFs(fsObj);
+
+  // Stub out creating a temp directory with a _known_ name.
+  base.sandbox.stub(temp, "mkdir").yields(null, tmpDir);
+
+  // Immediately call `close` with success exit.
+  stubs.spawnOn = base.sandbox.stub();
+  stubs.spawnOn.withArgs("error").returns();
+  stubs.spawnOn.withArgs("close").yields(0);
+
+  // Override the `npm pack` process to just fail / succeed.
+  stubs.spawn = base.sandbox.stub(childProc, "spawn").returns({
+    on: stubs.spawnOn
+  });
+
+  // Use our special hook to change the filesystem as if we expanded a
+  // real download.
+  base.sandbox.stub(Task.prototype, "_onExtracted", function (callback) {
+    base.mockFs(extractedObj);
+
+    return callback;
+  });
+
+  stubs.prompt = base.sandbox.stub(Prompt.prototype, "run").yields("dest");
+
+  return stubs;
+};
+/*eslint-enable max-statements*/
+
+describe("bin/builder-init", function () {
+
+  beforeEach(function () {
+    // Mock out unzipping.
+    base.sandbox.stub(zlib, "createUnzip").returns(new stream.PassThrough());
+
+    // Node `4`+ can't `require` from the mocked filesystem, so hackily
+    // approximate here.
+    base.sandbox.stub(Task.prototype, "_lazyRequire", function (mod) {
+      try {
+        return require(mod); // eslint-disable-line global-require
+      } catch (err) {
+        if (err.code === "MODULE_NOT_FOUND" && base.fileExists(mod)) {
+          return _eval(base.fileRead(mod));
+        }
+
+        throw err;
+      }
+    });
+  });
+
+  describe("non-init", function () {
+
+    it("displays help on no args", stdioWrap(function (done) {
+      run({ argv: ["node", "builder-init"] }, function (err) {
+        if (err) { return done(err); }
+
+        expect(process.stdout.write).to.be.calledWithMatch("builder-init [flags] <archetype>");
+
+        done();
+      });
+    }));
+
+    it("displays help on -h", stdioWrap(function (done) {
+      run({ argv: ["node", "builder-init", "-h"] }, function (err) {
+        if (err) { return done(err); }
+
+        expect(process.stdout.write).to.be.calledWithMatch("builder-init [flags] <archetype>");
+
+        done();
+      });
+    }));
+
+    it("displays version on -v", stdioWrap(function (done) {
+      run({ argv: ["node", "builder-init", "-v"] }, function (err) {
+        if (err) { return done(err); }
+
+        expect(process.stdout.write).to.be.calledWithMatch(pkg.version);
+
+        done();
+      });
+    }));
+
+  });
+
+  describe("errors", function () {
+
+    it("errors on missing init/ and no init.js", stdioWrap(function (done) {
+      mockFlow({});
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        expect(err).to.have.property("message").that.contains("init' directory not found");
+        done();
+      });
+    }));
+
+    it("errors on missing init/ with init.js", stdioWrap(function (done) {
+      mockFlow({
+        "init.js": "module.exports = {};"
+      });
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        expect(err).to.have.property("message").that.contains("init' directory not found");
+        done();
+      });
+    }));
+
+    it("errors on init/ not a directory", stdioWrap(function (done) {
+      mockFlow({
+        "init": "file, not a directory"
+      });
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        expect(err).to.have.property("message").that.contains("exists, but is not a directory");
+        done();
+      });
+    }));
+
+    it("errors when destination already exists", stdioWrap(function (done) {
+      mockFlow({
+        "init": {}
+      }, {
+        "dest": {} // Will collide with default destination.
+      });
+
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        expect(err).to.have.property("message").that.contains("dest already exists");
+        done();
+      });
+    }));
+
+    it("errors on failed npm pack download", stdioWrap(function (done) {
+      var stubs = mockFlow({
+        "init": {}
+      });
+
+      // Fake npm pack download error.
+      stubs.spawnOn.reset();
+      stubs.spawnOn.withArgs("error").returns();
+      stubs.spawnOn.withArgs("close").yields(1);
+
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        expect(err).to.have.property("message").that.contains("exited with error code: 1");
+        done();
+      });
+    }));
+
+    it("errors on invalid --prompts data", stdioWrap(function (done) {
+      mockFlow({
+        "init.js": "module.exports = " + JSON.stringify({
+          prompts: {
+            name: { message: "a name" }
+          }
+        }) + ";",
+        "init": {
+          "{{name}}.txt": "A <%= name %>."
+        }
+      });
+      run({ argv: ["node", "builder-init", "archetype", "--prompts=INVALID"] }, function (err) {
+        expect(err).to.have.property("message").that.contains("Prompt overrides loading failed");
+        done();
+      });
+    }));
+
+  });
+
+  describe(".npmignore and .gitignore complexities", function () {
+
+    it("errors on .npmignore collision", stdioWrap(function (done) {
+      mockFlow({
+        "init": {
+          ".npmignore": "",
+          "{{npmignore}}": ""
+        }
+      });
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        expect(err).to.have.property("message")
+          .that.contains("Encountered 1 file path conflict").and
+          .that.contains("npmignore");
+
+        done();
+      });
+    }));
+
+    it("errors on .gitignore collision", stdioWrap(function (done) {
+      mockFlow({
+        "init": {
+          ".gitignore": "",
+          "{{gitignore}}": ""
+        }
+      });
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        expect(err).to.have.property("message")
+          .that.contains("Encountered 1 file path conflict").and
+          .that.contains("gitignore");
+
+        done();
+      });
+    }));
+
+    it("errors on .gitignore and .npmignore collisions", stdioWrap(function (done) {
+      mockFlow({
+        "init": {
+          ".gitignore": "",
+          "{{gitignore}}": "",
+          ".npmignore": "",
+          "{{npmignore}}": ""
+        }
+      });
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        expect(err).to.have.property("message")
+          .that.contains("Encountered 2 file path conflicts").and
+          .that.contains("gitignore").and
+          .that.contains("npmignore");
+
+        done();
+      });
+    }));
+
+    it("expands .gitignore and excludes ignored files", stdioWrap(function (done) {
+      var stubs = mockFlow({
+        "init.js": "module.exports = " + JSON.stringify({
+          prompts: {
+            fileName: { message: "a file name" },
+            varName: { message: "a variable name" }
+          }
+        }) + ";",
+        "init": {
+          "{{gitignore}}": "coverage",
+          "coverage": {
+            "a-file": "shouldn't be copied"
+          },
+          "{{fileName}}.js": "module.exports = { <%= varName %>: 'foo' };"
+        }
+      });
+
+      // Note: These have to match prompt fields + `destination` in order.
+      stubs.prompt
+        .reset()
+        .onCall(0).yields("file-name")
+        .onCall(1).yields("myCoolVar")
+        .onCall(2).yields("dest");
+
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        if (err) { return done(err); }
+
+        expect(base.fileRead("dest/.gitignore")).to.contain("coverage");
+        expect(base.fileRead("dest/file-name.js")).to.contain("myCoolVar: 'foo'");
+        expect(base.fileExists("dest/coverage/a-file")).to.be.false;
+
+        done();
+      });
+    }));
+
+  });
+
+  describe("basic", function () {
+
+    it("allows no init.js and empty init/", stdioWrap(function (done) {
+      mockFlow({
+        "init": {}
+      });
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, done);
+    }));
+
+    it("allows no init.js with init/", stdioWrap(function (done) {
+      mockFlow({
+        "init": {
+          "foo.js": "module.exports = { foo: 42 };"
+        }
+      });
+
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        if (err) { return done(err); }
+
+        expect(base.fileRead("dest/foo.js")).to.contain("foo: 42");
+
+        done();
+      });
+    }));
+
+    it("initializes a basic project", stdioWrap(function (done) {
+      var stubs = mockFlow({
+        "init.js": "module.exports = " + JSON.stringify({
+          prompts: {
+            fileName: { message: "a file name" },
+            varName: { message: "a variable name" }
+          }
+        }) + ";",
+        "init": {
+          "{{npmignore}}": "coverage",
+          "{{gitignore}}": "coverage",
+          "README.md": "My readme",
+          "{{fileName}}.js": "module.exports = { <%= varName %>: 'foo' };",
+          "test": {
+            "client": {
+              "spec": {
+                "{{fileName}}.spec.js": "describe('<%= fileName %>');"
+              }
+            }
+          }
+        }
+      });
+
+      // Note: These have to match prompt fields + `destination` in order.
+      stubs.prompt
+        .reset()
+        .onCall(0).yields("file-name")
+        .onCall(1).yields("myCoolVar")
+        .onCall(2).yields("dest");
+
+      run({ argv: ["node", "builder-init", "mock-archetype"] }, function (err) {
+        if (err) { return done(err); }
+
+        expect(base.fileRead("dest/.npmignore")).to.contain("coverage");
+        expect(base.fileRead("dest/.gitignore")).to.contain("coverage");
+        expect(base.fileRead("dest/README.md")).to.contain("My readme");
+        expect(base.fileRead("dest/file-name.js")).to.contain("myCoolVar: 'foo'");
+        expect(base.fileRead("dest/test/client/spec/file-name.spec.js"))
+          .to.contain("describe('file-name');");
+
+        done();
+      });
+    }));
+
+    it("handles --prompts data", stdioWrap(function (done) {
+      mockFlow({
+        "init.js": "module.exports = " + JSON.stringify({
+          prompts: {
+            name: { message: "a name" }
+          }
+        }) + ";",
+        "init": {
+          "{{name}}.txt": "A <%= _.capitalize(name) %>."
+        }
+      });
+
+      var prompts = "--prompts='" + JSON.stringify({
+        name: "chester",
+        destination: "dest"
+      }) + "'";
+
+      run({ argv: ["node", "builder-init", "archetype", prompts] }, function (err) {
+        if (err) { return done(err); }
+
+        expect(base.fileRead("dest/chester.txt")).to.contain("A Chester.");
+
+        done();
+      });
+    }));
+
+  });
+
+});
