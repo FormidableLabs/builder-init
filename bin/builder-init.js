4893c30e5609bfcc5bfb14a1229da9e2d33eff62
Ryan Roemer <ryan@loose-bits.com>
2016-12-04 21:44:57 -0800

    Chore abstract archetype (#41)
    More abstraction for #37. Template engine in `lib/` is now completely independent of `bin/builder-init.js`.

--- bin/builder-init.js
+++ bin/builder-init.js
@@ -1,12 +1,57 @@
 #!/usr/bin/env node
 "use strict";
 
-var _ = require("lodash");
+var fs = require("fs");
+var path = require("path");
+
 var init = require("../lib/init");
 var pkg = require("../package.json");
 
+// Helpers
+var extend = function (base, obj) {
+  return Object.keys(obj).reduce(function (memo, key) {
+    memo[key] = obj[key];
+    return memo;
+  }, base);
+};
+
+var readJson = function (filePath, callback) {
+  fs.readFile(filePath, function (err, data) {
+    if (err) {
+      // Allow "not found" as empty data.
+      return void callback(err.code !== "ENOENT" ? err : null, {});
+    }
+
+    var json;
+    try {
+      json = JSON.parse(data);
+    } catch (jsonErr) {
+      return void callback(jsonErr);
+    }
+
+    callback(null, json);
+  });
+};
+
+var parallel = function (fns, callback) {
+  var results = {};
+  Object.keys(fns).forEach(function (key, index, keys) {
+    fns[key](function (err, data) {
+      // Update results object.
+      results[key] = data;
+
+      // Terminate on error or finished.
+      if (err || Object.keys(results).length === keys.length) {
+        callback(err, results); // eslint-disable-line callback-return
+        callback = function () {};
+      }
+    });
+  });
+};
+
+// Runner
 var run = module.exports = function (opts, callback) {
-  return init(_.merge({
+  return init(extend({
     script: "builder-init",
     version: pkg.version,
     initFile: "init.js",
@@ -14,6 +59,16 @@ var run = module.exports = function (opts, callback) {
       derived: {
         // Directory containing templates
         _templatesDir: function (data, cb) { cb(null, "init"); },
+
+        // Custom fields
+        archetype: function (data, cb) {
+          // TODO: Document `_extractedModulePath` in extracted project.
+          var extractedPath = data._extractedModulePath;
+          parallel({
+            package: readJson.bind(null, path.resolve(extractedPath, "package.json")),
+            devPackage: readJson.bind(null, path.resolve(extractedPath, "dev/package.json"))
+          }, cb);
+        },
 
         // Legacy names before underscored lib naming.
         // **Note**: Values from `lib/prompts.DEFAULTS` are resolved _before_
@@ -27,12 +82,11 @@ var run = module.exports = function (opts, callback) {
   }, opts), callback);
 };
 
+// Script
 if (require.main === module) {
   run(null, function (err) {
-    // TODO: REAL LOGGING
-    // https://github.com/FormidableLabs/builder-init/issues/4
+    // Try to get full stack, then full string if not.
     if (err) {
-      // Try to get full stack, then full string if not.
       console.error(err.stack || err.toString()); // eslint-disable-line no-console
     }
 
